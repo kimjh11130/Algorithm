6870b2d8ae322e111a537f68617a2fe8db53d8b6
kimjh11130 <kimjh11130@naver.com>
2024-01-10 10:19:22 +0900

    백준 15654번

---
+++ 실버 3/15654번.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n, m, i, arr[9], ar[9], vis[9];
+
+void back() {
+    if (m == i) {
+        for (int j = 1; j <= m; j++) {
+            printf("%d ", arr[j]);
+        }
+        printf("\n");
+    } else {
+        for (int j = 0; j < n; j++) {
+            if (!vis[j]) {
+                vis[j] = 1, arr[++i] = ar[j];
+                back();
+                vis[j] = 0, i--;
+            }
+        }
+    }
+}
+
+int comp(const void *a, const void *b) {
+    return *(int *) a - *(int *) b;
+}
+
+int main(void) {
+    scanf("%d %d", &n, &m);
+    for (int j = 0; j < n; j++) {
+        scanf("%d", &ar[j]);
+    }
+    qsort(ar, n, 4, comp);
+    back();
+}
