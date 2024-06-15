6cb8d49b6f0ec9892d84c4bf2f2ec4cf58c110d2
kimjh11130 <kimjh11130@naver.com>
2024-06-15 21:38:54 +0900

    백준 23306번
    binary는 호남선

---
+++ 실버 2/23306번.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,a,b;
+    cin >> n;
+    cout << "? 1"<<endl;
+    cin >> a;
+    cout << "? " << n <<endl;
+    cin >> b;
+    cout << "! "<< b-a<<endl;
+}
